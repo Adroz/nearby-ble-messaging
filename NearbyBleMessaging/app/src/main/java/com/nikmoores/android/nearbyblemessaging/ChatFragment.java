92a04923e10c5da20105394138f8924db707e334
Adroz <nicholas.k.moores@gmail.com>
2016-02-22 15:15:09 +1100

    Add GoogleApiClient lifecycle.

--- NearbyBleMessaging/app/src/main/java/com/nikmoores/android/nearbyblemessaging/ChatFragment.java
+++ NearbyBleMessaging/app/src/main/java/com/nikmoores/android/nearbyblemessaging/ChatFragment.java
@@ -2,17 +2,30 @@ package com.nikmoores.android.nearbyblemessaging;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
 
 /**
  * Fragment that allows for user to transmit messages to nearby devices, and to also receive
  * messages from them.
- *
+ * <p/>
  * Created by Nik on 21/02/2016.
  */
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String LOG_TAG = ChatFragment.class.getSimpleName();
+
+    private GoogleApiClient mGoogleApiClient;   // Google play services - for Nearby API
 
     public ChatFragment() {
     }
@@ -31,5 +44,45 @@ public class ChatFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        if (mGoogleApiClient.isConnected() && !getActivity().isChangingConfigurations()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(LOG_TAG, "GoogleApiClient connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG, "GoogleApiClient connection suspended, error: " + i);
+        // Disable any UI elements that rely on the API until it is reconnected. In this point,
+        // there is nothing to disable.
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(LOG_TAG, "GoogleApiClient connection failed.");
+        // Not handled as of yet. GoogleApiClient onConnectionFailed guideline found at:
+        // https://developers.google.com/android/guides/api-client#handle_connection_failures
+        Toast.makeText(getActivity(), "blah", Toast.LENGTH_SHORT).show();
     }
 }
